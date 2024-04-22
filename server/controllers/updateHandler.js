ed4032e017b728e6eafdb1af8a25720af73d0f19
artem <artemzhr11@gmail.com>
2024-04-22 21:45:19 +0300

    Backend_1.7.8

--- server/controllers/updateHandler.js
+++ server/controllers/updateHandler.js
@@ -8,7 +8,22 @@ const updateInfo = async (req, res) => {
             res.status(404).send(`Помилка. Користувач не знайден`) //@ Користувача з переданим ID не існує
             return
         } else {
-            const result = await loadUpdate(req.params.ID, req.params.name, req.params.mail, req.params.password, parseInt(req.params.sold), req.params.contactMail, req.params.phone, req.params.region, req.params.allowNotifications, req.params.instagram, req.params.telegram, req.params.image)
+            const { image } = req.body
+            const data = [
+                req.params.ID,
+                req.params.name,
+                req.params.mail,
+                req.params.password,
+                parseInt(req.params.sold),
+                req.params.contactMail,
+                req.params.phone,
+                req.params.region,
+                req.params.allowNotifications,
+                req.params.instagram,
+                req.params.telegram,
+                image
+            ]
+            const result = await loadUpdate(data)
             res.status(200).send(result) //@ Зміни до профіля були зроблені
             return
         }
