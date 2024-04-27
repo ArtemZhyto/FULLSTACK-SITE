f95be8a7faf6cb8c903c2f54b913a0c91b090648
Тимофей Иванченко <ivanchenko.tiv@gmail.com>
2024-04-27 20:12:24 +0300

    filter with any type of category

--- Quickbuyer/src/features/classic/productsFilter/productsFilter.js
+++ Quickbuyer/src/features/classic/productsFilter/productsFilter.js
@@ -58,39 +58,61 @@ export const productsFilter = (products, qstring, filters) => {
 						good.category === "Аксессуары" ||
 						good.category === "Футболки и блузки" ||
 						good.category === "Платья" ||
-						good.category === "Юбки и брюки"
+						good.category === "Юбки и брюки" ||
+						good.category === "Одежда"
 					)
 				case "Спорт и фитнес":
 					return (
 						good.category === "Тренажеры и оборудование" ||
 						good.category === "Спортивная одежда" ||
-						good.category === "Аксессуары"
+						good.category === "Аксессуары" ||
+						good.category === "Спорт и фитнес"
 					)
 				case "Дом и быт":
 					return (
 						good.category === "Мебель" ||
 						good.category === "Техника для дома" ||
-						good.category === "Декор и товары для дома"
+						good.category === "Декор и товары для дома" ||
+						good.category === "Дом и быт"
 					)
 				case "Красота и здоровье":
 					return (
 						good.category === "Уход за кожей" ||
 						good.category === "Уход за волосами" ||
-						good.category === "Здоровье и медицинские товары"
+						good.category === "Здоровье и медицинские товары" ||
+						good.category === "Красота и здоровье"
 					)
 				case "Электроника":
 					return (
 						good.category === "Смартфоны и гаджеты" ||
 						good.category === "Ноутбуки и компьютеры" ||
-						good.category === "Фото и видео"
+						good.category === "Фото и видео" ||
+						good.category === "Электроника"
 					)
 				case "Книги и развлечения":
 					return (
-						good.category === "Книги" || good.category === "Фильмы и музыка"
+						good.category === "Книги" ||
+						good.category === "Фильмы и музыка" ||
+						good.category === "Книги и развлечения"
+					)
+				case "Мужская одежда":
+					return (
+						good.category === "Верхняя одежда" ||
+						good.category === "Брюки и шорты" ||
+						good.category === "Аксессуары" ||
+						good.category === "Мужская одежда"
+					)
+				case "Женская одежда":
+					return (
+						good.category === "Платья" ||
+						good.category === "Футболки и блузки" ||
+						good.category === "Юбки и брюки" ||
+						good.category === "Аксессуары" ||
+						good.category === "Женская одежда"
 					)
 
 				default:
-					break
+					return good.category === filters.category
 			}
 		})
 }
